56c5574f9bf17322434cdfafcf8c146ff1b42a30
Niruban2810 <97875800+Niruban2810@users.noreply.github.com>
2022-02-11 14:31:03 +0530

    Add files via upload

---
+++ foreach.js
@@ -0,0 +1,5 @@
+var names = ["jerry", "tom", "chak", "new", "mini"];
+names.forEach(function1);
+function function1(currentValue, index) {
+   console.log("Index in array is: "+index + " ::  Value is: "+currentValue);
+}
